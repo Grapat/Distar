12b7b06ce0d89dbf7ce8c6d36f7dba2fd51ba68a
unknown <s6502041620026@email.kmutnb.ac.th>
2025-02-09 17:39:08 +0700

    ready

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended:false }));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 //controller
 const vegController = require('./controller/Vegetable_Controller');
@@ -16,5 +17,10 @@ app.use('/api/vegs', vegController);
 
 //listen
 app.listen(4005, () => {
-    console.log('server running on 4005')
-})
+    console.log('server running on 4005');
+});
+
+// Handle all other routes and serve the Vite-built frontend
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
