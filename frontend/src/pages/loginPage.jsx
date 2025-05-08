f5e9a4fb47214c208cba79a4bed9ae57ed1256d6
unknown <s6502041620026@email.kmutnb.ac.th>
2025-05-09 03:12:19 +0700

    todeploy3

--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -27,7 +27,7 @@ const LoginPage = () => {
     setError(null);
 
     try {
-      const response = await fetch(`${API}/api/auth/login`, {
+      const response = await fetch("http://localhost:4005/api/auth/login" || `${API}/api/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
